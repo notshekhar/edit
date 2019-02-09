c9ec6ed21fc051ca2bbf315f2dec7200464e64b2
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2019-02-09 19:19:42 +0530

    Update edit.js

--- edit.js
+++ edit.js
@@ -1,1 +1,15 @@
+let input = document.querySelector('#input')
+let output = document.querySelector('#output')
+let reset = document.querySelector('#rset')
+let doc = output.contentWindow.document
 
+input.onclick = () => {
+  let html = input.value 
+  doc.body.innerHTML = value
+}
+reset.onclick = () => {
+  let html = "<!DOCTYPE html>↵<html>↵<head>↵<title>↵↵</title>↵</head>↵<body>↵↵↵↵</body>↵</html>"
+  input.value = html
+}
+
+  
